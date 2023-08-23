a503ab0627f2a834d653e6a7e8a4786a93a7ab62
Solomon <solomonaz2022@gmail.com>
2023-08-23 06:22:14 +0000

    fewest number of coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""
-determining the fewest number of coins 
-"""
+
+""" Contains makeChange function"""
+
+
 def makeChange(coins, total):
-      """
+    """
     Returns: fewest number of coins needed to meet total
         If total is 0 or less, return 0
         If total cannot be met by any number of coins you have, return -1
     """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
-        return -1
-
-    return dp[total]
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
